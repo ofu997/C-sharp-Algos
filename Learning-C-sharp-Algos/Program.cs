dd66cce9fb1d7b8bb707730a4569601ed39c39a9
oliver fu <ofu997@gmail.com>
2021-04-27 22:16:50 -0700

    Add project files.

---
+++ Learning-C-sharp-Algos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_C_sharp_Algos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
